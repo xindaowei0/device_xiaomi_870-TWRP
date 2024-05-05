150a8a3c26ab19135ac332e0d9fb9d2e1894e970
辛道韦 <151035881+xindaowei0@users.noreply.github.com>
2024-05-06 07:07:41 +0800

    dagu Update

---
+++ twrp_dagu.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/alioth
+
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := alioth
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := elish
+PRODUCT_NAME := twrp_dagu
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi Pad 5 Pro 12.4
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Theme
+TW_STATUS_ICONS_ALIGN := center
+TW_Y_OFFSET := 91
+TW_H_OFFSET := -91
